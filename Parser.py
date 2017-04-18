d4ff88571a29fdc6997172bd25692c48ebf24f66
prajj <prajjalitadey@gmail.com>
2017-04-18 00:55:50 -0400

    created separate functions for parsing Airbnb data and Twitter data

--- Parser.py
+++ Parser.py
@@ -1,8 +1,12 @@
 import csv
 
 class Parser:
-    
-    def __init__(self):    
+
+    def __init__(self):
+        pass
+
+    def parseAirbnb(self):
+        # open reviews file & create reviews dictionary
         reviewsfile = open('jsons/nycreviews.csv', 'rb')
         rvreader = csv.reader(reviewsfile)
         reviews = defaultdict(list)
@@ -10,7 +14,8 @@ class Parser:
             listing_id = row[0]
             comment = row[5]
             reviews[listing_id].append(comment)
-       
+
+        # open listings file & create listings dictionary
         listingsfile = open('jsons/nyclistings.csv', 'rb')
         listingsreader = csv.reader(listingsfile)
         listings = defaultdict(list)
@@ -19,16 +24,16 @@ class Parser:
                 listing_id = row[0]
                 neighborhood = row[5]
                 listings[neighborhood].append(listing_id)
-        
-        
-    
+
+        # create listing_reviews dictionary that combines both listings and reviews
         listing_reviews = {}
         for neighborhood,listing_ids in listings.iteritems():
             nested_list = [reviews[listing] for listing in listing_ids if listing in reviews.keys()]
             listings_reviews[neighborhood] = [item for sublist in nested_list for item in sublist]
-         
-        #Combining list of reviews for each neighborhood, into a single string
-        #Also converting all the reviews text into UTF-8 encoding, to build the tf-idf later    
+
+
+        # Combining list of reviews for each neighborhood, into a single string
+        # Also converting all the reviews text into UTF-8 encoding, to build the tf-idf later
         for neighborhood in listings_reviews:
             if(len(listings_reviews[neighborhood])>1):
                 k=' '.join(listings_reviews[neighborhood])
@@ -41,10 +46,12 @@ class Parser:
                 if(len(listings_reviews[neighborhood])==0):
                     listings_reviews[neighborhood]='the' # i think empty text creates an error, hence this
         self.listings_reviews = listing_reviews
-        
+
+    def parseTwitter(self):
+        pass
+
     def getReviews(self):
         return self.listings_reviews
-    
+
     def __unicode__(self):
         return unicode(self.some_field) or u''
-                    
