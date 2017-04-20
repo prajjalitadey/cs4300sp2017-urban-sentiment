026aa61b27e0a9cd9ae788f05a1d76da2142a9ee
Lylla <ley23@cornell.edu>
2017-04-20 13:00:20 -0400

    style fixes / dynamic query / full stack enhancements [lylla]

--- project_template/test.py
+++ project_template/test.py
@@ -28,23 +28,14 @@ def find_similar(q):
 	return sorted(result, key=lambda tup: tup[0])
 
 def get_neighborhood_ranking(q):
-	# listing = {}
-	# listing['Wall Street'] = [Listing(1, 'great place close to wall street', 'Wall Street')]
-	# listing['Fordham'] = [Listing(2, 'shitty ass neighborhood', 'Fordham')]
-	# listing['Eastchester'] = [Listing(3, 'Westchester', 'Eastchester')]
-	# listing['Manhanttan'] = [Listing(4, 'yes I know its a borough', 'Manhattan')]
-
-	#transcripts = read_file(11)
-	query = "new york, outside of manhattan, quiet area, elderly people"
+	query = q
 	parser = Parser()
-	#parser.parseAirbnb("/Users/lyllayounes/Desktop/cs4300sp2017-urban-sentiment/jsons/test.csv")
 	parser.parseAirbnb("./jsons/test.csv")
 	listing = parser.getReviews()
 	output = Output(listing)
 	tups = output.getNaiveImpl(query)
 	tmp = []
-	tmp.append(["Query: "+query])
-	tmp.append(["-------------------------------------------------"])
+	tmp.append([query])
 
 	for item in tups:
 		tmp.append(item)
